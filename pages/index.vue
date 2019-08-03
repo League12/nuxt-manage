c2a945c48b293a76ec902abe6549b5c0fdd4cd98
League12 <280276366@qq.com>
2019-08-03 15:52:32 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    index
+    <ul>
+      <li v-for="data in datalist" :key="data">
+        {{ data }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+
+  export default {
+
+    data() {
+      return {
+        datalist: [111, 222, 333]
+      }
+    }
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
